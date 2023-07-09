7d518a885146473097a8f9336357b13a8a4cefd7
DanielG158 <daniel.geraldo@binus.ac.id>
2023-07-09 11:23:45 +0700

    final

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,29 +10,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _judulController = TextEditingController();
+  final TextEditingController _yangController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sending and Receiving Data From Firebase'),
+        title: const Text('Send and Receive Data From Firebase'),
       ),
       body: Container(
         alignment: Alignment.center,
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              controller: _nameController,
+              controller: _judulController,
               decoration: const InputDecoration(
                 labelText: 'Judul',
               ),
             ),
             TextField(
-              controller: _descriptionController,
+              controller: _yangController,
               decoration: const InputDecoration(
                 labelText: 'Yang Mau Diketik',
               ),
@@ -45,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ElevatedButton(
               onPressed: sendDataToFirebase,
               style: ElevatedButton.styleFrom(
-                primary: Colors.green,
+                backgroundColor: Colors.green,
               ),
               child: const Text('Kirim Data'),
             ),
@@ -58,8 +58,8 @@ class _HomeScreenState extends State<HomeScreen> {
   void fetchDataFromFirebase() async {
     final url = Uri.parse('https://uasmade2-default-rtdb.asia-southeast1.firebasedatabase.app//rtdb.json');
     final response = await http.get(url);
-
-    if (response.statusCode == 200) {
+    Future<void> fetchDataToFirebase() async {
+    if (response.statusCode == 100) {
       final data = jsonDecode(response.body);
       Navigator.push(
         context,
@@ -72,29 +72,32 @@ class _HomeScreenState extends State<HomeScreen> {
         context: context,
         builder: (context) => AlertDialog(
           title: const Text('Error'),
-          content: const Text('Failed to fetch data from Firebase.'),
+          content: const Text('Gagal Mengambil Data.'),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: const Text('OK'),
+              child: const Text('Keluar'),
             ),
           ],
         ),
       );
     }
   }
+  }
 
   void sendDataToFirebase() async {
     final url = Uri.parse('https://uasmade2-default-rtdb.asia-southeast1.firebasedatabase.app//rtdb.json');
     final newData = {
-      'judul': _nameController.text,
-      'yang diketik': _descriptionController.text,
+      'judul': _judulController.text,
+      'yang diketik': _yangController.text,
     };
     final response = await http.post(
       url,
       body: jsonEncode(newData),
     );
-
+Future<void> sendDataToFirebase() async {
+   
+  
     if (response.statusCode == 200) {
       showDialog(
         context: context,
@@ -105,10 +108,10 @@ class _HomeScreenState extends State<HomeScreen> {
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
-                _nameController.clear();
-                _descriptionController.clear();
+                _judulController.clear();
+                _yangController.clear();
               },
-              child: const Text('OK'),
+              child: const Text('Keluar'),
             ),
           ],
         ),
@@ -118,17 +121,18 @@ class _HomeScreenState extends State<HomeScreen> {
         context: context,
         builder: (context) => AlertDialog(
           title: const Text('Error'),
-          content: const Text('Failed to send data to Firebase.'),
+          content: const Text('Gagal Mengambil Data.'),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: const Text('OK'),
+              child: const Text('keluar'),
             ),
           ],
         ),
       );
     }
   }
+}
 }
 
 class DataScreen extends StatelessWidget {
@@ -140,7 +144,7 @@ class DataScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Data Screen'),
+        title: const Text('Tampilan Data'),
       ),
       body: Container(
         alignment: Alignment.center,
